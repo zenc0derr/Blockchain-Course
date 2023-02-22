fb7838e790537c469140d3c08aedb745f186cc74
Tejesh Kumar S <zenc0derr>
2023-02-23 00:06:33 +0530

    Learnt basic hardhat

--- Lesson 6: Hardhat Simple Storage/hardhat.config.js
+++ Lesson 6: Hardhat Simple Storage/hardhat.config.js
@@ -6,6 +6,7 @@ require("hardhat-gas-reporter");
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
 	defaultNetwork: "hardhat",
@@ -27,5 +28,9 @@ module.exports = {
 	},
 	gasReporter: {
 		enabled: true,
+		outputFile: "gas-report.txt",
+		noColors: true,
+		currency: "USD",
+		coinmarketcap: COINMARKETCAP_API_KEY,	
 	},
 };
